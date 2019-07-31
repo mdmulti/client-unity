0aab81ea7814fc73687a096322a6ef6b46e21d98
James Cahill <jcahill773@gmail.com>
2019-07-31 09:20:23 +0100

    ConnectionTest graphic initial

---
+++ Assets/MDMulti/Scripts/Panels/ConnectionTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MDMulti.Panels
+{
+    public class ConnectionTest : MonoBehaviour
+    {
+        public Image display;
+
+        //public Image incompleteGraphic;
+        public Sprite successfulGraphic;
+        public Sprite failedGraphic;
+
+        async void Start()
+        {
+            display.sprite = ((await RestHelper.ConnectionTest()) == true ? successfulGraphic : failedGraphic);
+        }
+    }
+
+}
