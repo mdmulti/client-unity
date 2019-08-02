d4363a3b33c89c5baf56ac08c91946d68c4c6fdd
James Cahill <jcahill773@gmail.com>
2019-08-02 11:28:34 +0100

    ConnectionTests - WAN

---
+++ Assets/MDMulti/Scripts/ConnectionTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Sockets;
+
+namespace MDMulti
+{
+    public class ConnectionTests
+    {
+        public static bool WAN()
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                /* 
+                 * Google Public DNS
+                 * (google-public-dns-a.google.com)
+                 * Open on 53/tcp (DNS)
+                 */
+                s.Connect("8.8.8.8", 53);
+                return s.Connected;
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
+            
+        }
+    }
+}
