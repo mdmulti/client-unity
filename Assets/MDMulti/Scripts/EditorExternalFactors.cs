1b671348387e84d906143196845c123dd84e5f82
James Cahill <jcahill773@gmail.com>
2019-08-31 10:03:22 +0100

    Add MultipleMethods to LAN Discovery Panel

--- Assets/MDMulti/Scripts/EditorExternalFactors.cs
+++ Assets/MDMulti/Scripts/EditorExternalFactors.cs
@@ -18,9 +18,35 @@ namespace MDMulti.Editor.Factors
         
         public static void OnServerFound(ServerDetails sd)
         {
-            if (!servers.Contains(sd)) servers.Add(sd);
             UnityEngine.Debug.Log("EDITOR_FACTORS_IP " + sd.IP);
             UnityEngine.Debug.Log("EDITOR_FACTORS " + servers.ToArray().Length);
+
+            // If the server does not exist in the list whatsoever then add it
+            if (!servers.Contains(sd)) {
+                servers.Add(sd);
+                UnityEngine.Debug.Log("A1");
+            } else
+            // The server exists in the list, so we will check to see if the DiscoveryMethod matches
+            {
+                ServerDetails found = null;
+                UnityEngine.Debug.Log("A2");
+
+                found = servers.Find(s => s.IP == sd.IP && s.Port == sd.Port && s.DiscoveryMethod != sd.DiscoveryMethod);
+
+
+                UnityEngine.Debug.Log("A2:1 " + found);
+                // The check
+                if (found != null)
+                {
+                    UnityEngine.Debug.Log("A3");
+                    // We got a match!
+                    // Now we can delete the old server details and add the updated version!
+                    servers.Remove(found);
+                    found.DiscoveryMethod = DiscoveryMethod.MultipleMethods;
+                    servers.Add(found);
+                }
+
+            }
         }
     }
 }
--- Assets/MDMulti/Scripts/LAN/Discovery/ServerFound.cs
+++ Assets/MDMulti/Scripts/LAN/Discovery/ServerFound.cs
@@ -16,7 +16,8 @@ namespace MDMulti.LAN.Discovery
     public enum DiscoveryMethod
     {
         Broadcast = 0,
-        Multicast = 1
+        Multicast = 1,
+        MultipleMethods = 9
     };
 
     public class ServerDetails : IEquatable<ServerDetails>
@@ -43,7 +44,7 @@ namespace MDMulti.LAN.Discovery
 
         public bool Equals(ServerDetails other)
         {
-            return other != null && other.IP == IP && other.Port == Port && other.DiscoveryMethod == DiscoveryMethod;
+            return other != null && other.IP == IP && other.Port == Port;
         }
 
         public override int GetHashCode()
