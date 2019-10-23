c052fc3cb1ebcd80433b5b6fb5edf8769320ec72
James Cahill <jcahill773@gmail.com>
2019-10-23 19:55:02 +0100

    More DebugScene Options

---
+++ Assets/Debug/DebugScene/Latest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MDMulti_DEBUG.DebugScene
+{
+    public class Latest : MonoBehaviour
+    {
+        public void Latest1()
+        {
+            Debug.Log("L");
+        }
+
+        public void Latest2()
+        {
+            Debug.Log("L2");
+        }
+    }
+}
---
+++ Assets/Debug/DebugScene/PeerConnection.cs
@@ -0,0 +1,26 @@
+﻿using MDMulti.LAN.Discovery;
+using UnityEngine;
+
+namespace MDMulti_DEBUG.DebugScene
+{
+    public class PeerConnection : MonoBehaviour
+    {
+
+        private ServerFoundEvent.serverFoundDel sfd;
+
+        public void AddListener()
+        {
+            sfd = new ServerFoundEvent.serverFoundDel(onServerFoundTest);
+            ServerFoundEvent.OnServerFound += sfd;
+        }
+
+        private async void onServerFoundTest(ServerDetails sfe)
+        {
+            Debug.LogError("L_INT");
+            Debug.LogError(await new PeerConnectionClient(sfe).IsValidPeer());
+            Debug.LogError("L_INT_REM_INPROG");
+            ServerFoundEvent.OnServerFound -= sfd;
+            Debug.LogError("L_INT_REM_DONE");
+        }
+    }
+}
