54e04e2921b67fd7181392e95d78fe18e9ba3641
James Cahill <jcahill773@gmail.com>
2019-11-24 10:00:26 +0000

    Add ConstantsTest to DebugScene
    Closes #8.

---
+++ Assets/Debug/DebugScene/Constants.cs
@@ -0,0 +1,14 @@
+﻿using MDMulti;
+using UnityEngine;
+
+namespace MDMulti_DEBUG.DebugScene
+{
+    public class Constants : MonoBehaviour
+    {
+        public void LogTest()
+        {
+            MDMulti.Constants.ConstantsObject c = ConstantsHelper.Get();
+            Debug.Log("ConstantsLogTest: Multicast Information\n" + c.Lan.Multicast.Address + ":" + c.Lan.Multicast.Port);
+        }
+    }
+}
